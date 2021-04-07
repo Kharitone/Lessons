6dda296a3339f2e02c47a7a2918959ac071ea543
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-04-07 10:46:38 +0000

    not clear

--- init-01/fix-an-error.php
+++ init-01/fix-an-error.php
@@ -2,6 +2,6 @@
 
 $var=elephant;
 
-# change the code to achieve same string on output as in comments
+# Change the code to achieve same string on output as in comments
 echo "<p>$var</p>";   // elephant
-echo "<p>$var</p>";   // "elephant"
+echo "<p>\"$var\"</p>";   // "elephant"
