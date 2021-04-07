d41a1cb4d957212da8aad8ae91d3973cc44c6ded
Oleg Sergiyuk <oleg.sergiyuk@gmail.com>
2021-04-07 14:11:49 +0000

    Homework: Lesson3

---
+++ init-03/connectVars.php
@@ -0,0 +1,3 @@
+<?php
+define(HOST, "google");
+define(PORT, 22);
---
+++ init-03/require.php
@@ -0,0 +1,5 @@
+<?php
+require "notexist.php";
+//include "hello.php";
+
+echo "<p style=\"color: blue; font-size:46px; font-family: Bookman;\"> Good! You made it!</p>";
